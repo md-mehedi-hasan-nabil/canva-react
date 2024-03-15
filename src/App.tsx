7ef0f578f9f62b574845b0ea9d872033cb0b4c4b
Md. Mehedi Hasan Nabil <mehedihasannabil49@gmail.com>
2024-03-15 06:57:03 +0600

    ResizeSidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,13 @@
-import { MouseEvent, useEffect } from "react";
-import { useRef } from "react"
+import ResizeSidebar from './components/ResizeSidebar/ResizeSidebar'
 
 export default function App() {
-  const sidebarRef = useRef(null);
-  const mainContentRef = useRef(null);
-  const resizeBarRef = useRef<<HTMLDivElement>(null);
-
-  function handleResize(e: MouseEvent) {
-    console.log(e)
-  }
-
-  useEffect(() => {
-    const resizeBar = resizeBarRef.current;
-
-    if (resizeBar) {
-      resizeBar.addEventListener("mousedown", handleResize);
-
-      return () => {
-        resizeBar.removeEventListener("mousedown", handleResize);
-      };
-    }
-  }, [])
-
-  console.log(resizeBarRef)
-
   return (
-    <div className="flex h-svh">
-      <aside className="w-40 h-full bg-slate-200 p-2">
-        dsds
-      </aside>
-      <div ref={resizeBarRef} className="w-1 transition select-none hover:cursor-ew-resize hover:bg-slate-500"></div>
-      <main className="p-2">
-        Main
-      </main>
+    <div>
+      <ResizeSidebar sidebar={<div>
+        Sidebar
+      </div>}>
+        Main Content
+      </ResizeSidebar>
     </div>
   )
 }
---
+++ src/components/ResizeSidebar/ResizeSidebar.tsx
@@ -0,0 +1,69 @@
+import { ReactNode, useEffect, useRef } from "react";
+
+export default function ResizeSidebar({ children, sidebar }: { children: ReactNode, sidebar: ReactNode }) {
+    const sidebarRef = useRef<HTMLDivElement>(null);
+    const resizeBarRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        let isResizing = false;
+        let startX: number;
+        let sidebarWidth: number;
+
+        const startResize = (e: MouseEvent) => {
+            if (sidebarRef.current) {
+                isResizing = true;
+                startX = e.clientX;
+                sidebarWidth = sidebarRef.current.offsetWidth;
+            }
+        };
+
+        const resize = (e: MouseEvent) => {
+            if (sidebarRef.current && isResizing) {
+                const width = sidebarWidth + e.clientX - startX;
+                console.log(width)
+                if (100 <= width) {
+                    sidebarRef.current.style.width = `${width}px`;
+                }
+            }
+        };
+
+        const stopResize = () => {
+            isResizing = false;
+        };
+
+        const handleMouseUp = () => {
+            if (isResizing) {
+                stopResize();
+            }
+        };
+
+        if (resizeBarRef.current) {
+            resizeBarRef.current.addEventListener("mousedown", startResize);
+            document.addEventListener("mousemove", resize);
+            document.addEventListener("mouseup", handleMouseUp);
+        }
+
+        return () => {
+            if (resizeBarRef?.current) {
+                resizeBarRef.current.removeEventListener("mousedown", startResize);
+                document.removeEventListener("mousemove", resize);
+                document.removeEventListener("mouseup", handleMouseUp);
+            }
+        };
+    }, []);
+
+    return (
+        <div className="flex h-svh">
+            <aside ref={sidebarRef} className="w-40 h-full bg-slate-200 p-2">
+                {sidebar}
+            </aside>
+            <div
+                ref={resizeBarRef}
+                className="w-1 transition select-none hover:cursor-ew-resize hover:bg-slate-500"
+            ></div>
+            <main className="p-2">
+                {children}
+            </main>
+        </div>
+    );
+}
